87e0522b9cf7350a824f1593e0aeb6059cf4b932
Marco Malavolti <marco.malavolti@gmail.com>
2020-06-15 18:50:44 +0200

    Updated JQuery scripts and css

--- web/index.php
+++ web/index.php
@@ -2,9 +2,9 @@
 <html>
    <head>
  
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.js"></script>
-      <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css"/>
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+      <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
 
       <script type="text/javascript" src="script.js"></script>
       <link href="style.css" rel="stylesheet" type="text/css" />
@@ -21,7 +21,7 @@
     </div>
     <hr>
     <div class="container">
-      <table id="eccstable" class="display" style="width:100%">
+      <table id="eccstable" class="display compact" style="width:100%">
         <thead>
             <tr>
                 <th></th>
